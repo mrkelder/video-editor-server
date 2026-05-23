579958a5dca7311e7fc38fe52fcd9bdc13f1058d
Vladyslav Aheiev <vageev06@gmail.com>
2026-05-22 22:46:41 -0400

    feat: add basic sign up controller

--- src/sign-up/sign-up.controller.ts
+++ src/sign-up/sign-up.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
+import { CreateUserDto } from './sign-up.dto';
+import type { CreateUserResponse } from './sign-up.types';
+import type { Response } from 'express';
 
 @Controller('sign-up')
-export class SignUpController {}
+export class SignUpController {
+  @Post()
+  @HttpCode(200)
+  createUser(
+    @Body() createUserDto: CreateUserDto,
+    @Res({ passthrough: true }) response: Response,
+  ): CreateUserResponse {
+    response.cookie('refreshToken', 'xxx.yyy.zzz', {
+      httpOnly: true,
+      secure: false, // TODO: add IS_PRODUCTION env variable
+      sameSite: true,
+    });
+    return { accessToken: 'xxx.yyy.zzz' };
+  }
+}
