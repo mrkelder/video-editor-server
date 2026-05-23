f2f03cd0ec7940230beb131e13eac67c720ff75c
Vladyslav Aheiev <vageev06@gmail.com>
2026-05-22 22:51:30 -0400

    feat: add sign up service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SignUpController } from './sign-up/sign-up.controller';
+import { SignUpService } from './sign-up/sign-up.service';
 
 @Module({
   imports: [],
   controllers: [AppController, SignUpController],
-  providers: [AppService],
+  providers: [AppService, SignUpService],
 })
 export class AppModule {}
