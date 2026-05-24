7ea486205a9ed4375d12004c7dc848c14a876c8a
Vladyslav Aheiev <vageev06@gmail.com>
2026-05-24 19:01:38 -0400

    feat: integrate sign up service with createUser method

--- src/sign-up/sign-up.controller.ts
+++ src/sign-up/sign-up.controller.ts
@@ -2,20 +2,26 @@ import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
 import { CreateUserDto } from './sign-up.dto';
 import type { CreateUserResponse } from './sign-up.types';
 import type { Response } from 'express';
+import { SignUpService } from './sign-up.service';
 
 @Controller('sign-up')
 export class SignUpController {
+  constructor(private signUpService: SignUpService) {}
+
   @Post()
   @HttpCode(200)
   createUser(
     @Body() createUserDto: CreateUserDto,
     @Res({ passthrough: true }) response: Response,
   ): CreateUserResponse {
-    response.cookie('refreshToken', 'xxx.yyy.zzz', {
+    const { accessToken, refreshToken } =
+      this.signUpService.getTokenCombination();
+
+    response.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: false, // TODO: add IS_PRODUCTION env variable
       sameSite: true,
     });
-    return { accessToken: 'xxx.yyy.zzz' };
+    return { accessToken };
   }
 }
