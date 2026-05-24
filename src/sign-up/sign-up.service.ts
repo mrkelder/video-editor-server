88ddca65be938a415704170d898e2fe278450154
Vladyslav Aheiev <vageev06@gmail.com>
2026-05-24 18:58:07 -0400

    feat: add getTokenCombination method

--- src/sign-up/sign-up.service.ts
+++ src/sign-up/sign-up.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import type { TokenCombination } from './sign-up.types';
 
 @Injectable()
-export class SignUpService {}
+export class SignUpService {
+  getTokenCombination(): TokenCombination {
+    return { accessToken: '', refreshToken: '' };
+  }
+}
