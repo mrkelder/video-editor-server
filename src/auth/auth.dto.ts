700e10885adfffb607f14c2a4c25386d33ba1b1c
Vladyslav Aheiev <vageev06@gmail.com>
2026-06-01 18:18:05 -0400

    feat: add properties to create user dto

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  readonly userName: string;
+  readonly password: string;
+  readonly inviteCode: string;
+
+  constructor(userName: string, password: string, inviteCode: string) {
+    this.userName = userName;
+    this.password = password;
+    this.inviteCode = inviteCode;
+  }
+}
