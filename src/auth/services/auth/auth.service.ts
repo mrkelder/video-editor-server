bb161b57e803c2feee3d5b13c409df9e2195ed4d
Vladyslav Aheiev <vageev06@gmail.com>
2026-06-05 22:35:48 -0400

    feat: make verifyRefreshToken return jwt token payload

--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -70,13 +70,14 @@ export class AuthService {
     return { accessToken, refreshToken };
   }
 
-  async verifyRefreshToken(refreshToken: string): Promise<void> {
+  async verifyRefreshToken(refreshToken: string): Promise<JwtTokenPayload> {
     try {
-      await this.jwtService.verifyAsync(refreshToken, {
-        secret: MOCK_JWT_SECRET,
-      });
+      const jwtTokenPayload =
+        await this.jwtService.verifyAsync<JwtTokenPayload>(refreshToken, {
+          secret: MOCK_JWT_SECRET,
+        });
 
-      return void 0;
+      return jwtTokenPayload;
     } catch {
       throw new Error('Refresh token invalid');
     }
