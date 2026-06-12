77a4a085b5992e2963ab5da3681728b009b4bacb
Vladyslav Aheiev <vageev06@gmail.com>
2026-06-11 20:57:38 -0400

    fix: rename envConfig to config

--- src/services/env/env.service.ts
+++ src/services/env/env.service.ts
@@ -3,13 +3,13 @@ import { EnvConfig } from './env.types';
 
 @Injectable()
 export class EnvService {
-  readonly envConfig: EnvConfig;
+  readonly config: EnvConfig;
 
   constructor() {
     const isProduction = process.env.IS_PRODUCTION;
     const isMockServer = process.env.IS_MOCK_SERVER;
 
-    this.envConfig = {
+    this.config = {
       isProduction: typeof isProduction === 'boolean' ? isProduction : false,
       isMockServer: typeof isMockServer === 'boolean' ? isMockServer : false,
     };
