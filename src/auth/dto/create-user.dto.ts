726eccb406e4fa8000dd06e0789be8e7c8126d15
Vladyslav Aheiev <vageev06@gmail.com>
2026-06-16 22:56:57 -0400

    fix: replace user name with email

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
-import { IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString() userName!: string;
+  @IsEmail() email!: string;
   @IsString() password!: string;
   @IsString() inviteCode!: string;
 }
