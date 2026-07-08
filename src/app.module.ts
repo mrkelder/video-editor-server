15c5493cd31d172fc0725a22b7a56e7c3bfd9d08
Vladyslav Aheiev <vageev06@gmail.com>
2026-07-07 21:23:04 -0400

    fix: move auth controller to controllers folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/services/auth/auth.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './controllers/auth/services/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { EnvService } from './services/env';
 import { ConfigModule } from '@nestjs/config';
